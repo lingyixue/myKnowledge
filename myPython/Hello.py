f4d24a50dd45216de66d8c237cf3434cc9688c3e
Jriver19 <jiahejiang@qq.com>
2017-10-13 10:29:23 +0800

    add Python folder and document

---
+++ myPython/Hello.py
@@ -0,0 +1,50 @@
+# This is my first Python program
+#  --From River.
+# You
+import math
+from math import pi
+
+# keyboard input
+text = input()
+print(text)
+
+# function
+def  cat_twice(part1, part2):
+    print("This is a function")
+    cat = part1 + part2
+    print(cat)
+# To end the function, you have to enter an empty line
+
+
+# call the method
+cat_twice(1,2)
+
+for i in range(3):
+    print("this is a FOR loop")
+
+n=5
+while n>0:
+    print(n)
+    n = n-1
+    
+
+# math
+print (math)
+radians=0.7
+height = math.sin(radians)
+print(height)
+
+print(pi)
+print(math.pi)
+
+
+# exercise 1, hello world.
+print("Hello, world!!")
+# exercise 2, add
+a = 2
+b = 3
+sum = a + b
+print(sum)
+# type
+print (type (a))
+
